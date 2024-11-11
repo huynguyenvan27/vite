dee9929faa55d09a2a4cf941883fc117e3caaf27
ThoLv <62795498+levantho1998@users.noreply.github.com>
2024-11-11 13:24:55 +0700

    Update App.tsx
    test cicd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>test cicd</h1>
       <h1>commit change</h1>
   
       <div className="card">
