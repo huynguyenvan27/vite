d1675a93e91f85afe73cc23aca45f473e83b0d38
huynguyenvan27 <89083694+huynguyenvan27@users.noreply.github.com>
2024-11-11 13:16:42 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
+      <h1>commit change</h1>
   
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>{count}</button>
