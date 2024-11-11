994d9f1e884279c7b496665a71f3e7cb0a23690c
huynguyenvan27 <89083694+huynguyenvan27@users.noreply.github.com>
2024-11-11 10:54:15 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setupTest.ts",
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
+    },
+  },
+  base: "/vite-app",
+  build: {
+    outDir: "build",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
