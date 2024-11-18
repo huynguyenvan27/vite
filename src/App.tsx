0d97f668129b5d41b7399970e220c2854308590a
huynguyenvan27 <89083694+huynguyenvan27@users.noreply.github.com>
2024-11-18 13:22:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>commit change1</h1>
+      <h1>commit change2</h1>
       <h1>commit change</h1>
   
       <div className="card">
